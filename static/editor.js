b97f70f57db7e44441cb727b115ab3eaaa115ee6
Yuri Iozzelli <y.iozzelli@gmail.com>
2026-01-11 18:32:24 +0100

    vendor alpinejs and easymde

--- static/editor.js
+++ static/editor.js
@@ -1,41 +1,18 @@
-import Alpine from "https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/module.esm.js";
-async function require(path) {
-	let _module = window.module;
-	let _exports = window.exports;
-	window.module = {};
-	window.exports = {};
-	await import(path);
-	let exports = module.exports;
-	window.module = _module; // restore global
-	window.exports = _exports; // restore global
-	return exports;
-}
-function load_css(url) {
-  let el = document.createElement("link");
-  el.href = url;
-  el.rel = "stylesheet";
-  document.head.appendChild(el);
-}
+import "./vendor/alpine.min.js";
+import "./vendor/easymde.min.js";
+import easyMDESheet from "./vendor/easymde.min.css" with { type: "css" };
 
-load_css("https://cdn.jsdelivr.net/npm/simplemde@1.11.2/dist/simplemde.min.css");
+document.adoptedStyleSheets = [easyMDESheet];
 
-let SimpleMDE = await require("https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js");
-
-window.Alpine = Alpine;
 Alpine.data("editor", () => ({
   mde: null,
   init() {
-    let mde = new SimpleMDE({
+    let mde = new EasyMDE({
       element: this.$el,
       autoDownloadFontAwesome: true,
+      forceSync: true,
     });
     mde.value(this.$el.text);
     this.mde = mde;
-
-    // Keep textarea value synced for htmx form serialization
-    mde.codemirror.on('change', () => {
-      this.$el.value = mde.value();
-    });
   },
 }));
-Alpine.start();
